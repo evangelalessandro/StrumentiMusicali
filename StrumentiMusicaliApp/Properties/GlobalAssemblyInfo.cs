a0a315c8f7979fa0b566dc92181fa62c2beffa41
evangelalessandro <evangel.alessandro@gmail.com>
2019-05-14 17:11:39 +0200

    Aseemblu info

--- StrumentiMusicaliApp/Properties/GlobalAssemblyInfo.cs
+++ StrumentiMusicaliApp/Properties/GlobalAssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("StrumentiMusicali.Library")]
+[assembly: AssemblyTitle("StrumentiMusicali.App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cavallo Pazzo Inc.")]
-[assembly: AssemblyProduct("StrumentiMusicali.Library")]
-[assembly: AssemblyCopyright("Copyright © Cavallo Pazzo Inc. 2018")]
+[assembly: AssemblyProduct("StrumentiMusicali.App")]
+[assembly: AssemblyCopyright("Copyright © Cavallo Pazzo Inc. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
-[assembly: Guid("114b9db5-6a31-4d89-bbb8-7ae7dd1a25c6")]
+[assembly: Guid("e6a4d0f6-dc39-431e-a562-ac0c4465bea4")]
 
 // Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.9")]
+[assembly: AssemblyFileVersion(version: "1.0.1.9")]

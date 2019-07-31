a0cc11db8bf235d2c07d5a82ddd71fc522123970
evangelalessandro <evangel.alessandro@gmail.com>
2019-07-31 11:11:02 +0200

    fix configuarazione nlog

--- SpostaBackupInFtp/Program.cs
+++ SpostaBackupInFtp/Program.cs
@@ -1,4 +1,7 @@
-﻿using StrumentiMusicali.ftpBackup.Backup;
+﻿using NLog;
+using NLog.Targets;
+using StrumentiMusicali.ftpBackup.Backup;
+using StrumentiMusicali.Library.Repo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,14 @@ namespace SpostaBackupInFtp
 {
     class Program
     {
+        internal static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         static int Main(string[] args)
         {
+           
+            ConfigureNLog();
+
+            _logger.Warn("Avvio del processo di spostamento backup");
+
             using (var back = new BackupManager())
             {
                 if (back.Manage())
@@ -25,6 +34,38 @@ namespace SpostaBackupInFtp
             Success = 0,
             Error = 1,
 
+        }
+
+        public static void LogMethod(string level, string message, string exception, string stacktrace, string classLine)
+        {
+            try
+            {
+                using (var uof = new UnitOfWork())
+                {
+                    uof.EventLogRepository.Add(new StrumentiMusicali.Library.Entity.EventLog()
+                    { TipoEvento = level, Errore = message, DataCreazione = DateTime.Now, InnerException = exception, StackTrace = stacktrace, Class = classLine });
+
+                    uof.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(@"Errore nel salvataggio log\errore! " + Environment.NewLine + ex.Message);
+            }
+        }
+
+        private static void ConfigureNLog()
+        {
+            MethodCallTarget target = new MethodCallTarget();
+            target.ClassName = typeof(Program).AssemblyQualifiedName;
+            target.MethodName = "LogMethod";
+            target.Parameters.Add(new MethodCallParameter("${level}"));
+            target.Parameters.Add(new MethodCallParameter("${message}"));
+            target.Parameters.Add(new MethodCallParameter("${exception:format=tostring,Data:maxInnerExceptionLevel=10}"));
+            target.Parameters.Add(new MethodCallParameter("${stacktrace}"));
+            target.Parameters.Add(new MethodCallParameter("${callsite}"));
+
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
         }
 
     }
